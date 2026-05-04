273dea3cf9116b87ac13cba5acea309caf464297
dentheroblox <eduverso4@gmail.com>
2026-05-04 10:04:42 +0800

    add models route

--- eduv_backend_mysql/routes/aiTutorRoutes.js
+++ eduv_backend_mysql/routes/aiTutorRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { chat } = require('../controllers/aiTutorController');
+const { chat, listModels } = require('../controllers/aiTutorController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+router.get('/models', listModels);
 router.post('/chat', protect, chat);
 
 module.exports = router;
